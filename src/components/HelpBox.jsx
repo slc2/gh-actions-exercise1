631a823b367fb1af76e75f0daab2285879f1628f
slc2 <sbspfun@yahoo.com>
2025-07-12 13:32:01 -0400

    introducing lint rule and intentional issue in HelpBox.jsx that lint should find

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -2,6 +2,8 @@ import { string } from 'prop-types';
 
 import './HelpBox.css';
 
+const notUsed = 'This file is not used in the project.';
+
 function HelpBox({ title, text }) {
   return (
     <article className="help-box">
